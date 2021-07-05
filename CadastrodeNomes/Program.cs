46c9294c47513ef09d1753fba043f2f6e10178ac
AnaCarolinaFortunato <anacarolinafortunatosantos@gmail.com>
2021-07-05 10:56:55 -0300

    Adicionado validações

--- CadastrodeNomes/Program.cs
+++ CadastrodeNomes/Program.cs
@@ -21,15 +21,37 @@ namespace CadastrodeNomes
                         Console.WriteLine("Qual o nome do cliente? ");
                         Console.WriteLine("Nome: ");
 
-                        clientes.Add(Console.ReadLine());
+                        var nome = Console.ReadLine();
+
+                        var verificarcliente = clientes.FindIndex(c => c.Equals(nome));
+                        
+                        if (verificarcliente > -1)
+                        {
+                            Console.WriteLine("Já existe cliente com esse nome! ");
+                            Console.WriteLine("                                 ");
+                        }
+                       
+                        else if (string.IsNullOrWhiteSpace(nome))
+                        {
+                            Console.WriteLine("Nome invalido!Não deixar espaços vazio!");
+                            Console.WriteLine("                                 ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cliente adicionado com sucesso! ");
+                            Console.WriteLine("                                 ");
+                            clientes.Add(nome);
+                        }
 
 
                         break;
 
                     case "2":
                         Console.WriteLine("-------Alteração de cadastro------");
+                        Console.WriteLine("                                 ");
                         Console.WriteLine("Qual o nome do cliente a ser alterado ?");
                         Console.WriteLine("Nome: ");
+                        Console.WriteLine("                                 ");
 
 
                         var nomeantigo = Console.ReadLine();
@@ -39,11 +61,13 @@ namespace CadastrodeNomes
                         if (clienteachado > -1)
                         {
                             Console.WriteLine("Escreva nome do cliente novo");
+                            Console.WriteLine("Nome: ");
                             clientes[clienteachado] = Console.ReadLine();
                         }
                         else
                         {
                             Console.WriteLine("Não foi achado cliente com esse nome. ");
+                            Console.WriteLine("                                 ");
                         }
 
 
@@ -53,8 +77,9 @@ namespace CadastrodeNomes
                     case "3":
 
                         Console.WriteLine("-----------------Lista de clientes--------------");
+                        Console.WriteLine("                                 ");
 
-                        
+
                         foreach (string cliente in clientes)
                         {
 
@@ -65,6 +90,7 @@ namespace CadastrodeNomes
 
                     case "4":
                         Console.WriteLine("-----------------Remover cliente da lista-----------------");
+                        Console.WriteLine("                                 ");
 
                         Console.WriteLine("Qual o nome do cliente a ser removido? ");
                         Console.WriteLine("Nome: ");
@@ -78,10 +104,12 @@ namespace CadastrodeNomes
                             clientes.RemoveAt(procuracliente);
 
                             Console.WriteLine("Cliente removido com sucesso");
+                            Console.WriteLine("                                 ");
                         }
                         else
                         {
                             Console.WriteLine("Não foi achado cliente com esse nome. ");
+                            Console.WriteLine("                                 ");
                         }
 
                         break;
@@ -103,12 +131,13 @@ namespace CadastrodeNomes
         public static string Opcaousuario()
         {
             Console.WriteLine("------------Sistema de cadastro de clientes----------");
+            Console.WriteLine("                                 ");
             Console.WriteLine("Escolha a opção desejada: ");
             Console.WriteLine("1-Incluir Cliente");
             Console.WriteLine("2-Alterar cadastro de cliente");
             Console.WriteLine("3-Exibir lista de clientes");
             Console.WriteLine("4-Excluir cadastro de cliente");
-            Console.WriteLine("5- Sair");
+            Console.WriteLine("5-Sair");
 
             var opcoes = Console.ReadLine();
 
